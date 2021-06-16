f1f5afa3fdb3141b43431f9bde4f1f4623c84557
William Davis <williamjsdavis+gitkraken@berkeley.edu>
2021-06-16 14:31:50 -0700

    Test Travis
    Test triggering passing test on TravisCI

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,10 @@
 using ConditionalMoments
 using Test
 
-my_fun(2,4)
+my_fun(3,4)
 
 @testset "ConditionalMoments.jl" begin
     @test my_fun(2,3) == 7
     @test my_fun(2,4) == 8
+    @test my_fun(3,4) == 10
 end
