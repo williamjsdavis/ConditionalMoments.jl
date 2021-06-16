60af715128a9da9333fcafa349fb2eb8dac58353
William Davis <williamjsdavis+gitkraken@berkeley.edu>
2021-06-16 14:49:49 -0700

    Test Codecov and AppVeyor
    Test triggering Codecov and AppVeyor

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,11 @@
 using ConditionalMoments
 using Test
 
-my_fun(3,4)
+my_fun(10,10)
 
 @testset "ConditionalMoments.jl" begin
     @test my_fun(2,3) == 7
     @test my_fun(2,4) == 8
     @test my_fun(3,4) == 10
+    @test my_fun(10,10) == 30
 end
