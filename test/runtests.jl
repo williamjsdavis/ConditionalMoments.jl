0a45779cdc3ff5e5393dcafabf3068eded47f71b
william davis <williamjsdavis@berkeley.edu>
2021-06-17 16:28:35 -0700

    Add basic tests for moment calulations

--- test/runtests.jl
+++ test/runtests.jl
@@ -22,15 +22,19 @@ end
 ## Moments
 
 τ_indices = [1,3,4]
-bin_edges = LinRange(-2,2,4)
+bin_edges = LinRange(-2,2,5)
 histogram_settings = MomentHistogramSettings(τ_indices,bin_edges)
 
 @testset "Moment settings" begin
     @test histogram_settings.n_time_points == 3
-    @test histogram_settings.n_evaluation_points == 4
+    @test histogram_settings.n_evaluation_points == 5
 end
 
 moments = Moments(ob, histogram_settings)
-@testset "Moments" begin
+@testset "Moment calculations" begin
     @test moments.observation.npoints == 6
+    @test size(moments.M1) == ((3,4))
+    @test size(moments.M2) == ((3,4))
+    @test moments.M1[1,4] == 1.5
+    @test moments.M2[1,4] == 2.25
 end
