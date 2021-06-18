40fb9be29f72e4d286644c0c5852866c233899bf
william davis <williamjsdavis@berkeley.edu>
2021-06-17 21:51:08 -0700

    Started drift and noise functions

---
+++ src/driftnoisefunctions.jl
@@ -0,0 +1,27 @@
+abstract type FitSettings end
+
+struct InvertSettings <: FitSettings end
+
+#TODO: Add other fit settings
+
+abstract type StochasticProcess end
+
+struct WhiteNoiseProcess{S<:Real,T<:AbstractVector{S}}
+    drift::T
+    noise::T
+    errors::T
+    moments::Moments
+    settings::InvertSettings
+end
+
+function WhiteNoiseProcess(moments::Moments,settings::InvertSettings)
+    drift, noise, errors = makef()
+    return WhiteNoiseProcess(drift,noise,errors,moments,settings)
+end
+
+function estimateProcess(moments::Moments,i::Integer;settings::InvertSettings)
+    τ_vector = moments.observation.dt * moments.evaluation_points
+    drift = moments.M1[i,:] ./ τ_vector
+    noise = moments.M2[i,:] ./ τ_vector
+    return drift, noise, errors
+end
