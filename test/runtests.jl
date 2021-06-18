2778a93d695077622a2c84141204bf6720d04418
william davis <williamjsdavis@berkeley.edu>
2021-06-17 20:56:56 -0700

    Add larger test

--- test/runtests.jl
+++ test/runtests.jl
@@ -35,8 +35,42 @@ end
 const moments = Moments(ob, histogramSettings)
 @testset "Moment calculations" begin
     @test moments.observation.npoints == 6
-    @test size(moments.M1) == ((3,4))
-    @test size(moments.M2) == ((3,4))
+    @test size(moments.M1) == (3,4)
+    @test size(moments.M2) == (3,4)
     @test moments.M1[1,4] == 1.5
     @test moments.M2[1,4] == 2.25
 end
+
+## Larger test
+
+function large_test()
+    t = 0:0.001:200
+    dt = t.step.hi
+    X = exp.(-t) .*
+        cumsum(exp.(t) .* (sqrt(dt)*pushfirst!(randn(length(t)-1),0)))
+    ob = Observation(X,dt)
+
+    @testset "Larger observation" begin
+        @test ob[ob .> 3] == X[X .> 3]
+        @test ob[(ob .> 3)'] == X[X .> 3]
+        @test ob[:] == X
+        @test ob.dt == dt
+        @test ob.npoints == length(t)
+    end
+
+    τ_indices = 1:20
+    τ_vec = dt*τ_indices
+    bin_edges = LinRange(-1,1,10)
+    histogramSettings = HistogramSettings(τ_indices,bin_edges)
+    moments = Moments(ob, histogramSettings)
+
+    M1_τ = moments.M1 ./ τ_vec
+
+    @testset "Larger moments" begin
+        @test moments.observation.npoints == length(t)
+        @test size(moments.M1) == (length(τ_indices),length(bin_edges)-1)
+        @test size(moments.M2) == (length(τ_indices),length(bin_edges)-1)
+    end
+end
+
+large_test()
